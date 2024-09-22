0e95d904ef98571b0cc260955a93e8873be85685
Prathap-4903 <prathap4903@gmail.com>
2024-09-22 23:55:18 +0530

    Input Field Added

--- client/src/screens/Signin_Signup/Sign_In.jsx
+++ client/src/screens/Signin_Signup/Sign_In.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Login_Styles.css";
 import LogoMonkey from "../../assets/icons/monkey-icon-b.png"
 import LogoText from "../../assets/icons/BingeWatch Text Black.png"
 
@@ -10,10 +11,12 @@ const Sign_In = () => {
       <img src={LogoMonkey} alt="Logo" width="40" height="40" className='mt-2 ml-2'/>
       <img src={LogoText} width="180" height="34.32" className='mt-3'/>
     </div>
-    <div>
-      
+    <div className="flex">
+      <label className="scale-[.60]">
+        <input type="text" className="h-[80px] w-[400px] px-6 text-[26px] bg-white font-geist-medium font-[300] border-2 rounded-lg border-black outline-none"/>
+        <span className="input-text font-geist-semi text-[27px] text-black absolute left-0 top-5 mx-6 px-2 transition duration-200 ease-in-out rounded-3xl ">Email</span>
+      </label>
     </div>
-      
     </>
   )
 }
