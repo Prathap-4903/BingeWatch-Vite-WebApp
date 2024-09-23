6a9aa64e613b94ff674588a964b051de20e1d442
Prathap-4903 <prathap4903@gmail.com>
2024-09-23 19:40:48 +0530

    Login Page Work Added

--- client/src/screens/Signin_Signup/Sign_In.jsx
+++ client/src/screens/Signin_Signup/Sign_In.jsx
@@ -29,29 +29,34 @@ const Sign_In = () => {
       <img src={LogoText} width="180" height="34.32" className='mt-3'/>
     </div>
 
-    <div className='flex justify-center items-center h-screen flex-col'>
-
-      <div className='bg-white p-8 border-black rounded-3xl border-2'>
-
-      <div className="flex">
-        <label className="scale-[.60]">
-          <input required type="text" className="peer h-[80px] w-[400px] px-6 text-[26px] bg-white font-geist-medium font-[300] border-2 rounded-lg border-black outline-none"/>
-          <span className="input-text absolute font-geist-semi text-[25px] text-black absolute left-0 top-5 mx-6 px-2 transition-all duration-200 ease-in-out transform ">Email</span>
-        </label>
+    <div className='flex justify-center items-center h-screen'>
+      <div className='flex flex-col bg-white pt-4 px-4 pb-4 border-black rounded-3xl border-[2.5px] space-y-2'>
+        <h1 className='font-geist-bold text-2xl'>Login</h1>
+        {/*<div className='absolute w-[35px] h-[4px] bg-black rounded-full'></div>*/}
+        <p className='font-geist-medium font-[375] '>Grab Your Popcorn and Join The Party!</p>
+        <div className='flex-col justify-center items-center'>
+        <div className='flex flex-col space-y-3'>
+          <div className='w-full space-y-1'>
+            <label htmlFor="floating_outlined_e" className='font-geist-semi'>Enter Email : </label>
+            <div class="relative w-full">
+              <input type="text" id="floating_outlined_e" class="block font-geist-medium font-light px-2.5 pb-2.5 pt-4 text-[20px] text-black bg-transparent rounded-lg border-2 border-black appearance-none focus:outline-none focus:ring-0 focus:border-black peer" placeholder="" />
+              <label for="floating_outlined_e" class="absolute font-geist-semi text-sm text-black duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Email</label>
+            </div>
+          </div>
+          <div className='w-full space-y-1'>
+            <label htmlFor="floating_outlined_p" className='font-geist-semi'>Enter Password : </label>
+            <div class="relative w-full">
+              <input type="text" id="floating_outlined_p" class="block font-geist-medium px-2.5 pb-2.5 pt-4 text-[20px] text-black bg-transparent rounded-lg border-2 border-black appearance-none focus:outline-none focus:ring-0 focus:border-black peer" placeholder="" />
+              <label for="floating_outlined_p" class="absolute font-geist-semi text-sm text-black duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Password</label>
+            </div>
+          </div>
+          <button className='font-geist-bold text-white bg-black border-2 border-black py-1 rounded-xl'>Sign In</button>
+        </div>
+        
+        
+        </div>
       </div>
-
-      <div className="flex">
-        <label className="scale-[.60]">
-          <input required value={maskedPassword} onChange={handlePasswordChange} type="text" className="peer h-[80px] w-[400px] px-6 pt-[15px] text-[30px] bg-white font-geist-medium font-[300] border-2 rounded-lg border-black outline-none"/>
-          <span className="input-password absolute font-geist-semi text-[25px] text-black absolute left-0 top-5 mx-6 px-2 transition-all duration-200 ease-in-out transform ">Password</span>
-        </label>
-      </div>
-
-      </div>
-
     </div>
-    
-    
     </>
   )
 }
