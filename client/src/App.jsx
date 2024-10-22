0c53db509e0ddc6ab314c26f92acf66fe5155329
Prathap-4903 <prathap4903@gmail.com>
2024-10-22 23:35:49 +0530

    Added Welcome Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Sign_In from './screens/Signin_Signup/Sign-In/Sign_In'
 import Sign_up from './screens/Signin_Signup/Sign-up/Sign_up'
 import Home_Page from './screens/Home_Page/Home_Page'
+import Welcome from './screens/Welcome_Page/Welcome';
 
 function App() {
 
   return (
     <>
       <Routes>
+        <Route path="/" element={<Welcome />} />
         <Route path="/sign-in" element={<Sign_In />} />
         <Route path="/sign-up" element={<Sign_up />} />
         <Route path="/home" element={<Home_Page />} />
---
+++ client/src/screens/Welcome_Page/Welcome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <>
+    <div className='Main-Screen w-full h-screen flex justify-center items-center'>
+        <h1>Welcome To BingeWatch - Stream Movies With Friends!</h1>  
+    </div>
+        
+    </>
+  )
+}
+
+export default Welcome
