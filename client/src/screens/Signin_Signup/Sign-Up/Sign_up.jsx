f9525fe1f1ad4c687d053442681b5f7ba469c10c
Prathap-4903 <prathap4903@gmail.com>
2024-10-23 23:59:03 +0530

    Adding Input Component

--- client/src/screens/Signin_Signup/Sign-Up/Sign_up.jsx
+++ client/src/screens/Signin_Signup/Sign-Up/Sign_up.jsx
@@ -68,6 +68,7 @@ const Sign_up = () => {
                     <label htmlFor="name" className="absolute font-geist-semi text-sm text-black duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Name</label>
                 </div>
               </div> */}
+              <Input IdName={"name"} InputLabel={"Enter Name : "} InputName={"Name"} changeEvent={(e) => setName(e.target.value)} />
               <div className="w-full space-y-1">
                 <label htmlFor="username" className="font-geist-semi">Enter Username : </label>
                 <div className="relative w-full">
