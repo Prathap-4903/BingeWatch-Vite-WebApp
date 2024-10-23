415056d4e881f9d25e361904678a12f05df9e545
Prathap-4903 <prathap4903@gmail.com>
2024-10-24 00:01:48 +0530

    Adding Input Component

--- client/src/screens/Signin_Signup/Sign-Up/Sign_up.jsx
+++ client/src/screens/Signin_Signup/Sign-Up/Sign_up.jsx
@@ -70,13 +70,13 @@ const Sign_up = () => {
               </div> */}
               <Input IdName={"name"} InputLabel={"Enter Name : "} InputName={"Name"} changeEvent={(e) => setName(e.target.value)} />
               <Input IdName={"username"} InputLabel={"Enter Username : "} InputName={"Username"} changeEvent={(e) => setUsername(e.target.value)} />
-              <div className="w-full space-y-1">
+              {/* <div className="w-full space-y-1">
                 <label htmlFor="username" className="font-geist-semi">Enter Username : </label>
                 <div className="relative w-full">
                   <input required type="text" id="username" className="block w-full font-geist-medium font-light px-2.5 pb-2.5 pt-4 text-[20px] text-black bg-transparent rounded-lg border-2  peer focus:border-r-4 focus:border-b-4 border-black appearance-none focus:outline-none focus:ring-0 focus:border-black placeholder-transparent" placeholder="" onChange={(e) => setUsername(e.target.value)} />
                   <label htmlFor="username" className="absolute font-geist-semi text-sm text-black duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Username</label>
                 </div>
-              </div>
+              </div> */}
               <div className="w-full space-y-1">
                 <label htmlFor="email" className="font-geist-semi">Enter Email : </label>
                 <div className="relative w-full">
