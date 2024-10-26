6f9c59382111b7d5fdfd1b496d0333643210499c
Prathap-4903 <prathap4903@gmail.com>
2024-10-27 00:35:52 +0530

    Added Input Component

--- client/src/screens/Signin_Signup/Sign-In/Sign_In.jsx
+++ client/src/screens/Signin_Signup/Sign-In/Sign_In.jsx
@@ -57,14 +57,7 @@ const Sign_In = () => {
             <div className="input-body flex flex-wrap space-y-3 justify-center items-center w-full">
               <div className="flex flex-wrap space-y-3 w-full">
                 <div className="input-container space-y-3 w-full">
-                  {/* <div className="w-full space-y-1">
-                <label htmlFor="email" className="font-geist-semi">Enter Email : </label>
-                <div className="relative w-full">
-                  <input type="text" id="email" className="block w-full font-geist-medium font-light px-2.5 pb-2.5 pt-4 text-[20px] text-black bg-transparent rounded-lg border-2 focus:border-r-4 focus:border-b-4 border-black appearance-none focus:outline-none focus:ring-0 focus:border-black peer" placeholder="" required onChange={(e)=> setEmail(e.target.value)} />
-                  <label htmlFor="email" className="absolute font-geist-semi text-sm text-black duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Email</label>
-                </div>
-              </div> */}
-                  <Input IdName={"email"} InputLabel={"Enter Email : "} InputName={" Email"} changeEvent={(e)=> setEmail(e.target.value)}/>
+                  <Input IdName={"email"} InputLabel={"Enter Email : "} InputType={"email"} InputName={" Email"} changeEvent={(e)=> setEmail(e.target.value)}/>
                   <div className="w-full space-y-1">
                     <label htmlFor="password" className="font-geist-semi">
                       Enter Password :{" "}
