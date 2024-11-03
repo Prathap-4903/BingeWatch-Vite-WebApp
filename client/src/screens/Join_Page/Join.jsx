95ed7443900b2d35689da2194e607d99d557ebc8
Prathap-4903 <prathap4903@gmail.com>
2024-11-04 02:14:18 +0530

    Started Working on Join

--- client/src/screens/Join_Page/Join.jsx
+++ client/src/screens/Join_Page/Join.jsx
@@ -1,8 +1,24 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Join = () => {
   return (
-    <div>Join</div>
+    <>
+      <div className='host-screen w-full h-screen bg-white flex justify-center items-center '>
+        <Card className="w-[480px] ">
+          <CardHeader>
+            <CardTitle className="font-geist-bold text-[20px] ">Room ID</CardTitle>
+            <CardDescription className="font-geist-semi text-[14px]" >Share this code with your homies!</CardDescription>
+            <CardContent></CardContent>
+            <CardContent className="font-geist-medium text-[18px] w-full ">{code}</CardContent>
+            <CardFooter className="flex justify-center">
+              <Button className="w-[180px]">Start Stream</Button>
+            </CardFooter>
+          </CardHeader>
+        </Card>  
+      </div>
+    </>
   )
 }
 
