f9d8dfe79b2e5be191e0659821e97bbb724672c3
Prathap-4903 <prathap4903@gmail.com>
2024-11-08 19:27:23 +0530

    Solved The SignIn Issue

--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -25,7 +25,7 @@ auth.post("/sign-in", async(req,res) => {
     const{email, password} = req.body;
     const user = await UserModel.findOne({email});
     if(!user){
-        return res.status(401).json({message: "Invalid Email or Password!"})
+        return res.status(401).json({message: "Invalid Email!"})
     }
 
     const validPassword = await bcrypt.compare(password, user.password)
@@ -33,10 +33,14 @@ auth.post("/sign-in", async(req,res) => {
         return res.status(401).json({message: "Invalid Password!"})
     }
 
-    const token = jwt.sign({email: user.email}, process.env.KEY, {expiresIn: '1h'})
-    // res.cookie('token', token, {httpOnly: true, maxAge: 86400000})
-    res.cookie("token", token, { httpOnly: true, maxAge: 3600000 })
-    return res.status(200).json({message: "Login Successful!"})
+    // const token = jwt.sign({email: user.email}, process.env.KEY, {expiresIn: '1h'})
+    // // res.cookie('token', token, {httpOnly: true, maxAge: 86400000})
+    // res.cookie('token', token, { httpOnly: true, maxAge: 360000 })
+    // return res.json({status: true, message: "Login Successful!"})
+    
+    const token = jwt.sign({username: user.username}, process.env.KEY, {expiresIn: '1h'})
+    res.cookie('token', token, {httpOnly: true, maxAge: 360000})
+    return res.json({status: true, message: "Login Successfully"})
 
 })
 
