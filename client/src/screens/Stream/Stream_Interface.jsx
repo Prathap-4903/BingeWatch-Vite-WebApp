826362305b9141387c08bf303178c2a5b035339d
Prathap-4903 <prathap4903@gmail.com>
2024-11-08 23:22:43 +0530

    Working on Stream Interface

--- client/src/screens/Stream/Stream_Interface.jsx
+++ client/src/screens/Stream/Stream_Interface.jsx
@@ -5,20 +5,20 @@ const Stream_Interface = () => {
   return (
     <>
     <div className='stream-screen w-full h-screen bg-[#1a1a1a] flex justify-center items-center gap-[5px] p-[6px] '>
-      <div className='left-part w-[800px] h-[669.45px] flex flex-col gap-[25px] flex-1 justify-center items-center bg-[#1f1f1f] border-solid border-[1px] border-[#666] rounded-[40px] '>
-        <div className='stream-video w-[895.56px] h-[480px] bg-[#1a1a1a] border-solid border-[1px] border-[#666] rounded-[25px] '></div>
-        <div className='bottom-part flex w-[853.28px] h-[204px] '>
-          <div className='bottom-left w-[652.39px] h-[204px] '>
-            <div className='controls flex w-[489.04] h-[80px] bg-[#1f1f1f] gap-[125.18px] '>
+      <div className='left-part w-[800px] h-[669.45px] flex flex-col flex-1 justify-center items-center bg-[#1f1f1f] border-solid border-[1px] border-[#666] rounded-[40px] '>
+        <div className='stream-video w-[895.56px] h-[480px] m-[7px] bg-[#1a1a1a] border-solid border-[1px] border-[#666] rounded-[32px] '></div>
+        <div className='bottom-part bg-slate-500 flex w-[895.56px] h-[204px] gap-[10px] '>
+          <div className='bottom-left bg-slate-50 flex flex-col gap-[15px] w-[652.39px] h-[204px] justify-start items-center pl-[12px]  '>
+            <div className='controls bg-red-950 flex w-[489.04] h-[80px] mt-[25px] gap-[125.18px] '>
               <div className='camera w-[60px] h-[60px] bg-[#292929] rounded-full border-solid border-[1px] border-[#666] '></div>
               <div className='mic w-[60px] h-[60px] bg-[#292929] rounded-full border-solid border-[1px] border-[#666] '></div>
               <div className='share-screen w-[60px] h-[60px] bg-[#292929] rounded-full border-solid border-[1px] border-[#666] '></div>
             </div>
-            <div className='reactions w-[652.39px] h-[60px] bg-[#292929] rounded-[25px] border-solid border-[1px] border-[#666] '></div>    
+            <div className='reactions w-[552.39px] h-[60px] bg-[#292929] rounded-[25px] border-solid border-[1px] border-[#666] '></div>    
           </div>
-          <div className='bottom-right w-[159.12px] h-[204px] '>
-            <div className='poll w-full h-[80px] bg-[#292929] rounded-[20px] border-solid border-[1px] border-[#666] '></div> 
-            <div className='file w-full h-[80px] bg-[#292929] rounded-[20px] border-solid border-[1px] border-[#666] '></div> 
+          <div className='bottom-right w-[243.17px] h-[204px] flex flex-col gap-[16px] justify-center items-end bg-green-500 pr-[12px] '>
+            <div className='poll w-[159.12px] h-[80px] bg-[#292929] rounded-[20px] border-solid border-[1px] border-[#666] '></div> 
+            <div className='file w-[159.12px] h-[80px] bg-[#292929] rounded-[20px] border-solid border-[1px] border-[#666] '></div> 
           </div>
         </div>
       </div>
