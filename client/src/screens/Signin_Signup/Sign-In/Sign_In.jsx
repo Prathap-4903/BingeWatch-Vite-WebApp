4eb8fb12d5471a04728d3ba93b4d676fc89b1273
Prathap-4903 <prathap4903@gmail.com>
2024-11-14 23:18:32 +0530

    Made Changes in SignIn

--- client/src/screens/Signin_Signup/Sign-In/Sign_In.jsx
+++ client/src/screens/Signin_Signup/Sign-In/Sign_In.jsx
@@ -75,7 +75,7 @@ const Sign_In = () => {
                       <label htmlFor="password" className="absolute font-geist-semi text-sm text-black duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Password</label>
                       <div className="absolute inset-y-0 right-5 flex items-center pl-3 cursor-pointer z-50">
                         <div onClick={() => setVisible(!visible)} className="flex items-end justify-end justify-items-end cursor-pointer">
-                          {visible ? <EyeClosed size={27}/> : <Eye size={27}/>}
+                          {visible ? <Eye size={27}/> : <EyeClosed size={27}/>}
                         </div>
                       </div>
                     </div>
