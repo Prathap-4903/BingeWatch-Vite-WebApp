0e15332d04d3febb77b6f023b4e4b1d627d04f9d
Prathap-4903 <prathap4903@gmail.com>
2024-11-20 15:08:04 +0530

    Working on Backend

--- server/function/uuid.js
+++ server/function/uuid.js
@@ -1,10 +1,21 @@
-import express from "express";
-import { v4 } from "uuid";
+// import express from "express";
+// import { v4 } from "uuid";
 
-const uuid = express.Router();
+// const uuid = express.Router();
 
-uuid.get("/host", (req, res) => {
-    res.send(v4())
-})
+// uuid.get("/host", (req, res) => {
+//     res.send(v4())
+// })
 
-export { uuid }
+// export { uuid }
+
+// const express = require('express');
+// const { v4 } = require('uuid');
+
+// const uuid = express.Router();
+
+// uuid.get("/host", (req, res) => {
+//     res.send(v4());
+// })
+
+// module.exports = { uuid };
