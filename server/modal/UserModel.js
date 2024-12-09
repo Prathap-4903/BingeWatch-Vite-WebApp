fcefd77f2dc0dc5ede944f0d896641d89680da7b
Prathap <prathap4903@gmail.com>
2024-12-09 22:59:30 +0530

    Update UserModel.js

--- server/modal/UserModel.js
+++ server/modal/UserModel.js
@@ -4,8 +4,8 @@ const UserSchema = new mongoose.Schema({
     name:{type: String, required: true},
     username:{type: String, required: true, unique: true},
     email:{type: String, required: true, unique: true},
-    password:{type: String, required: true},
-    confirmPassword:{type: String, required: true},
+    password:{type: String, required: true, minimum: 8},
+    confirmPassword:{type: String, required: true, minimum: 8},
     picture:{type: String}
 })
 UserSchema.set('timestamps', true);
