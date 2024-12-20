6ac0d97c13efbb5b6aa382904cc82535c5938c7b
Prathap <prathap4903@gmail.com>
2024-12-20 23:44:18 +0530

    Update Welcome.jsx

--- client/src/screens/Welcome_Page/Welcome.jsx
+++ client/src/screens/Welcome_Page/Welcome.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom"
-import { Button } from "@/components/ui/button"
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 const Welcome = () => {
 
