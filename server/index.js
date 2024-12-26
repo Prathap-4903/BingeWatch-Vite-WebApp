cfed76f9aae5ed4782235f59eeddb8c2b864c9b3
Prathap <prathap4903@gmail.com>
2024-12-26 23:13:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,8 @@ const server = createServer(app);
 const io = new Server(server, {
     cors: {
         origin: ["http://localhost:5173"],
-        credentials: true
+        credentials: true,
+        methods: ['GET', 'POST']
     }
 });
 app.use(express.json());
