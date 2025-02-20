a2f4bc1a64acf2ec82e85e2c626c27cd0987abe6
Prathap-4903 <prathap4903@gmail.com>
2025-02-20 22:25:46 +0530

    Joining Stream Glitch Resolved

--- client/src/screens/Join_Page/Join.jsx
+++ client/src/screens/Join_Page/Join.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import socket from '@/components/UI_Elements/socket';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
@@ -9,36 +9,47 @@ import { toast } from "@/hooks/use-toast";
 import UseUserStore from '@/store/UserStore';
 
 const Join = () => {
-  const [roomId, setRoomId] = useState();
+  const [roomId, setRoomId] = useState("");
   const { username } = UseUserStore();
   const nav = useNavigate();
 
+  useEffect(() => {
+    socket.on('join-room-pending', () => {
+      toast({
+        title: "Request Sent To Host!",
+        description: "Waiting for approval..."
+      });
+    });
+
+    socket.on('join-room-approved', () => {
+      nav(`/stream/${roomId}`);
+      toast({
+        title: "Request Approved By Host!",
+        description: "Enjoy The Stream..."
+      });
+    });
+
+    socket.on('join-room-rejected', () => {
+      toast({
+        variant: "destructive",
+        title: "Request Rejected By Host!",
+        description: "You can try again later."
+      });
+    });
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      socket.off('join-room-pending');
+      socket.off('join-room-approved');
+      socket.off('join-room-rejected');
+    };
+  }, [roomId]);
+
   //Functions
   const handleJoinRoom = () => {
+    console.log("Button Clicked!");
     if(roomId) {
       socket.emit('join-room', roomId, username);
-      socket.on('join-room-pending', () => {
-        toast({
-          title: "Request Sent To Host!",
-          description: "Waiting for approval..."
-        });
-      });
-
-      socket.on('join-room-approved', () => {
-          nav(`/stream/${roomId}`);
-          toast({
-            title: "Request Approved By Host!",
-            description: "Enjoy The Stream..."
-          });
-      });
-    
-      socket.on('join-room-rejected', () => {
-        toast({
-          variant: "destructive",
-          title: "Request Rejected By Host!",
-          description: "You can try again later."
-        });
-      });
     }
   };
 
@@ -67,3 +78,5 @@ const Join = () => {
 }
 
 export default Join;
+
+//  px-8 py-2 rounded-[6px] bg-black hover:bg-neutral-800 text-white text-[14px] font-geist-semi
