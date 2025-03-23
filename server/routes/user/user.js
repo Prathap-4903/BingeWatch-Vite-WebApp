346c67262b9afada44eb94646269b5508c0e96cf
Prathap-4903 <prathap4903@gmail.com>
2025-03-23 20:21:04 +0530

    Added User Details Route For Username

--- server/routes/user/user.js
+++ server/routes/user/user.js
@@ -5,23 +5,42 @@ import UserModel from '../../models/UserModel.js';
 const userApi = express.Router();
 
 //Send User Details To The Frontend
-userApi.get('/:id', verifyUser, async (req, res) => {
-    try {
-        const { id } = req.params;
-        const user = await UserModel.findById(id, '-password -createdAt -updatedAt');
-        if (!user) {
-            return res.status(404).json({ status: false, message: "User not found!" });
-        }
-        const userData = {
-            ...user._doc,
-            picture: `${req.protocol}://${req.get('host')}/${user.picture}`
-        };
-        // Send user details
-        return res.status(200).json({ status: true, data: userData, message: "User Data Send Successfully!" });
-    } catch(err) {
-        console.log(err);
-        return res.status(500).json({ status: false, message: "Server Error" });
+userApi.get('/id/:id', verifyUser, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await UserModel.findById(id, '-password -createdAt -updatedAt');
+    if (!user) {
+        return res.status(404).json({ status: false, message: "User not found!" });
     }
-})
+    const userData = {
+        ...user._doc,
+        picture: `${req.protocol}://${req.get('host')}/${user.picture}`
+    };
+    // Send user details
+    return res.status(200).json({ status: true, data: userData, message: "User Data Send Successfully!" });
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json({ status: false, message: "Server Error" });
+  }
+});
+
+userApi.get('/username/:username', verifyUser, async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await UserModel.findOne({ username }, '-password -createdAt -updatedAt');
+    if (!user) {
+        return res.status(404).json({ status: false, message: "User not found!" });
+    }
+    const userData = {
+        ...user._doc,
+        picture: `${req.protocol}://${req.get('host')}/${user.picture}`
+    };
+    // Send user details
+    return res.status(200).json({ status: true, data: userData, message: "User Data Send Successfully!" });
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json({ status: false, message: "Server Error" });
+  }
+});
 
 export { userApi };
