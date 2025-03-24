64323431d36023f977204f2ad647fc3f32ddbeb9
Prathap-4903 <prathap4903@gmail.com>
2025-03-24 23:32:32 +0530

    Updated Reaction_Tab.jsx

--- client/src/components/Stream_Elements/Reaction_Tab.jsx
+++ client/src/components/Stream_Elements/Reaction_Tab.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 const Reaction_Tab = () => {
     return (
         <>
-          <div className='reactions w-[552.39px] h-[60px] bg-[#292929] rounded-[25px] border-solid border-[1px] border-[#666] '></div>
+          <div className='reactions w-[552.39px] h-[60px] bg-[#292929] rounded-[25px] border border-solid border-[#666] '>
+            <div className='w-full h-full flex justify-center items-center gap-10 text-[24px] '>
+              <button>🔥</button>
+              <button>😂</button>
+              <button>😍</button>
+              <button>🍿</button>
+              <button>😱</button>
+              <button>😭</button>
+              <button>❤️</button>
+            </div>
+          </div>
         </>
     )
 }
