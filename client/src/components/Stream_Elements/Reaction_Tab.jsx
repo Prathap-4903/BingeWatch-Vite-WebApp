dad05bc2be66a354c08ee2c5c369ddac5a8f293e
Prathap-4903 <prathap4903@gmail.com>
2025-03-24 23:08:12 +0530

    Modified Reaction_Tab.jsx

---
+++ client/src/components/Stream_Elements/Reaction_Tab.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reaction_Tab = () => {
+    return (
+        <>
+          <div className='reactions w-[552.39px] h-[60px] bg-[#292929] rounded-[25px] border-solid border-[1px] border-[#666] '></div>
+        </>
+    )
+}
+
+export default Reaction_Tab;
