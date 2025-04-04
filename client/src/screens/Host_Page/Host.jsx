6c162eb01b60d5fb91e4caff41b828a3653bc9d5
Prathap-4903 <prathap4903@gmail.com>
2025-04-04 11:36:53 +0530

    Finished Host Page Responsive

--- client/src/screens/Host_Page/Host.jsx
+++ client/src/screens/Host_Page/Host.jsx
@@ -6,8 +6,10 @@ import socket from '@/components/functions/socket';
 import useUserStore from "@/store/UserStore";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
+import LogoMonkey from "@/assets/icons/monkey-icon-b.png";
+import LogoText from "@/assets/icons/BingeWatch Text Black.png";
 
-const Host = () => {
+const New_Host = () => {
   const { username } = useUserStore();
   const [roomId, setRoomId] = useState('');
   const nav = useNavigate();
@@ -43,15 +45,19 @@ const Host = () => {
 
   return (
     <>
+      <div className="logo-container flex cursor-pointer m-2">
+        <img src={LogoMonkey} alt="Logo" width="40" height="40" className="mt-1 ml-2" />
+        <img src={LogoText} width="180" height="34.32" className="mt-2" />
+      </div>
+
       <div className='host-screen w-full h-screen bg-white flex justify-center items-center '>
-        <Card className="w-[480px] ">
+        <Card className="min-w-[320px] max-w-[400px] md:min-w-[400px] md:max-w-[480px] mb-28">
           <CardHeader>
-            <CardTitle className="font-geist-bold text-[20px] ">Room ID</CardTitle>
-            <CardDescription className="font-geist-semi text-[14px]" >Share this code with your homies!</CardDescription>
-            <CardContent></CardContent>
-            <CardContent className="font-geist-medium text-[18px] w-full ">{roomId}</CardContent>
+            <CardTitle className="font-geist-bold text-[16px] md:text-[20px] ">Room ID</CardTitle>
+            <CardDescription className="font-geist-semi text-[12px] md:text-[14px]" >Share this code with your homies!</CardDescription>
+            <CardContent className="font-geist-medium text-[13px] md:text-[16px] w-full h-16 pt-6 px-0 flex justify-center items-center ">{roomId}</CardContent>
             <CardFooter className="flex justify-center">
-              <Button onClick={handleCreateRoom} className="w-[180px]">Start The Stream</Button>
+              <Button onClick={handleCreateRoom} className="w-[120px] md:w-[180px] font-geist-bold text-[12px]">Start The Stream</Button>
             </CardFooter>
           </CardHeader>
         </Card>  
@@ -60,4 +66,4 @@ const Host = () => {
   )
 }
 
-export default Host;
+export default New_Host;
