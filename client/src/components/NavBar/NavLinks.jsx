91bce97f4f32d3c0e7ff1d8821cc1b2b91ba9e98
Prathap-4903 <prathap4903@gmail.com>
2025-04-17 23:59:12 +0530

    Updated NavLinks.jsx

--- client/src/components/NavBar/NavLinks.jsx
+++ client/src/components/NavBar/NavLinks.jsx
@@ -8,8 +8,8 @@ const NavLinks = () => {
       <div className='flex items-center gap-16 mr-6 font-geist-bold font-bold'>
         <a href="#" className='hover:underline'>FRIENDS</a>
         <a href="#" className='hover:underline'>GROUPS</a>
-        <a href="#" className='hover:underline'>CONTACT</a>
-        <a href="#" className='hover:underline'>ABOUT US</a>
+        <a href="/contact" className='hover:underline'>CONTACT</a>
+        <a href="/about-us" className='hover:underline'>ABOUT US</a>
       </div>
     </>
   )
